a83fa78f3ee163df8b62cb44ef2b2d825734d076
Grey3636 <92727230+Grey3636@users.noreply.github.com>
2026-06-03 16:24:05 +0700

    реализована проверка описания товаров

--- src/test/java/com/saucedemo/appmanager/ChecHelper.java
+++ src/test/java/com/saucedemo/appmanager/ChecHelper.java
@@ -14,7 +14,7 @@ public class ChecHelper {
         this.driver=driver;
     }
 
-     public void checFilterPrice(String filter){
+    public void checFilterPrice(String filter){
 
          List<WebElement> elementList = driver.findElements(By.className("inventory_item_price")); //ищем все елементы в которых хронятся цены
          List<Double> valueElement = new ArrayList<>(); //создаем список для значений элкментов
@@ -38,7 +38,7 @@ public class ChecHelper {
 
      }
 
-     public void checFilterName(String filter){
+    public void checFilterName(String filter){
 
         List<WebElement> elementList = driver.findElements(By.className("inventory_item_name")); //ищем все елементы в которых хронятся название товара
         List<String> valueElement = new ArrayList<>(); //создаем список для названий товара
@@ -58,6 +58,44 @@ public class ChecHelper {
             System.out.println("не выбран фильтр");
         }
 
+    }
+
+    public void checProductDescription() {
+        int countProduct=driver.findElements(By.className("inventory_item_name")).size();
+
+        System.out.println("Всего таваров на странице " + countProduct);
+
+        for (int i=0; i<countProduct;i++){
+
+            System.out.println("Товар №"+(i+1));
+
+            Object[] itemArray=new Object[3];
+            Object[] itemArrayDescriptionPage=new Object[3];
+
+            itemArray[0]=driver.findElements(By.className("inventory_item_name")).get(i).getText();
+            itemArray[1]=driver.findElements(By.className("inventory_item_desc")).get(i).getText();
+            itemArray[2]=driver.findElements(By.className("inventory_item_price")).get(i).getText();
+            System.out.println("ОР:");
+            System.out.println(itemArray[0]);
+            System.out.println(itemArray[1]);
+            System.out.println(itemArray[2]);
+
+            driver.findElements(By.className("inventory_item_name")).get(i).click();
+
+            itemArrayDescriptionPage[0]=driver.findElement(By.cssSelector(".inventory_details_name.large_size")).getText();
+            itemArrayDescriptionPage[1]=driver.findElement(By.cssSelector(".inventory_details_desc.large_size")).getText();
+            itemArrayDescriptionPage[2]=driver.findElement(By.cssSelector(".inventory_details_price")).getText();
+            System.out.println("ФР:");
+            System.out.println(itemArrayDescriptionPage[0]);
+            System.out.println(itemArrayDescriptionPage[1]);
+            System.out.println(itemArrayDescriptionPage[2]);
+
+            Assertions.assertArrayEquals(itemArray,itemArrayDescriptionPage);
+
+            System.out.println("");
+
+            driver.findElement(By.id("back-to-products")).click();
+        }
     }
 
 
--- src/test/java/com/saucedemo/testing/TestProductDescription.java
+++ src/test/java/com/saucedemo/testing/TestProductDescription.java
@@ -10,39 +10,12 @@ public class TestProductDescription extends TestBase{
 
     @Test
     public void testProductDescription() throws Exception{
+
         app.login("standard_user", "secret_sauce");
-
-
         app.getNavigationHelper().goTo("allIteems");
-
-
-        Object[] itemArray=new Object[3];
-        Object[] itemArrayDescriptionPage=new Object[3];
-
-
-        itemArray[0]=app.driver.findElement(By.className("inventory_item_name")).getText();
-        itemArray[1]=app.driver.findElement(By.className("inventory_item_desc")).getText();
-        itemArray[2]=app.driver.findElement(By.className("inventory_item_price")).getText();
-        System.out.println(itemArray[0]);
-        System.out.println(itemArray[1]);
-        System.out.println(itemArray[2]);
-
-
-        app.driver.findElement(By.className("inventory_item_name")).click();
-
-
-        itemArrayDescriptionPage[0]=app.driver.findElement(By.cssSelector(".inventory_details_name.large_size")).getText();
-        itemArrayDescriptionPage[1]=app.driver.findElement(By.cssSelector(".inventory_details_desc.large_size")).getText();
-        itemArrayDescriptionPage[2]=app.driver.findElement(By.cssSelector(".inventory_details_price")).getText();
-        System.out.println(itemArrayDescriptionPage[0]);
-        System.out.println(itemArrayDescriptionPage[1]);
-        System.out.println(itemArrayDescriptionPage[2]);
-
-
-        Assertions.assertArrayEquals(itemArray,itemArrayDescriptionPage);
-
-
-        app.getNavigationHelper().goTo("bactToProducts");
+        app.getChecHelper().checProductDescription();
     }
+
+
 }
 
